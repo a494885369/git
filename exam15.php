8185041acf59f96a6120ecd73c2bc22a6bf04753
a494885369 <494885369@qq.com>
2018-11-16 08:38:14 +0800

    fd

---
+++ exam15.php
@@ -0,0 +1,23 @@
+<?php
+function FirstNotRepeatingChar($str)
+ {
+     $a = array(); //1、两个数组，一个存字母，一个存字母出现的次数
+     $b = array();
+     for($i=0;$i<strlen($str);$i++){
+         if(!isset($a[$str[$i]])){ //2、isset函数的使用
+             $a[$str[$i]] = 1;
+             $b[$str[$i]] = $i;
+         }else{
+             $a[$str[$i]] ++;
+         }
+     }
+     foreach($a as $k=>$v){
+         if($v==1){
+             return $b[$k];
+         }
+     }
+     return -1;
+ }
+$str="aaabaac";
+echo FirstNotRepeatingChar($str) ;
+?>
